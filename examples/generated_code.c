c57ad0ca807a152e7f2dc5a34daffda62c8ea070
Philipp Roshchin <r.phil@yandex.ru>
2026-01-04 00:41:44 +0300

    update

---
+++ examples/generated_code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+int main(void);
+
+int main(void) {
+    double a;
+    a = sqrt(16);
+    double b;
+    b = (((1 + 1) / 10) + 100);
+    if (((a > b) && (1 > 0))) {
+        printf("%d\n", 100);
+    }
+    return 0;
+}
---
+++ examples/main.p
@@ -0,0 +1,21 @@
+cimport <stdio.h>
+cimport <stdlib.h>
+cimport <string.h>
+cimport <stdbool.h>
+cimport <math.h>
+
+
+
+def main() -> int:
+    
+    var a: float = @sqrt(16)
+
+
+    var b: float = (1 + 1) / 10 + 100
+
+
+    if a > b and 1 > 0:
+        print(100)
+
+
+    return 0
