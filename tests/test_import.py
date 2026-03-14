4315d0048daac6006c18f7d9b5706114e25d8a24
Philipp Roshchin <r.phil@yandex.ru>
2026-03-14 15:23:26 +0300

    updated test

--- tests/test_import.py
+++ tests/test_import.py
@@ -7,11 +7,14 @@ cimport <stdio.h>
 cimport <stdlib.h>
 cimport <string.h>
 cimport <stdbool.h>
+cimport "my_header.h"
+import "./module.p"
 """
 
     C = """#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include "my_header.h"
 """
     run(P, C)
